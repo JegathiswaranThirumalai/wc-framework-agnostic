c7884125e39f24e58c01c744944fa5f06f7b7644
JegathiswaranThirumalai <165312440+JegathiswaranThirumalai@users.noreply.github.com>
2024-05-22 14:28:55 +0530

    first commit

---
+++ container/src/DataTableComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createComponent} from '@lit/react';
+import DataTable  from 'cases/dist/data-table';
+
+export const DataTableComponent = createComponent({
+  tagName: 'data-table',
+  elementClass: DataTable,
+  react: React,
+  events: {
+    onchange: 'change',
+  },
+});
+
+export default DataTableComponent;
